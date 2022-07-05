7d607d004c02553c4d19abaedea5dfbe9f00bf07
thekakimzhanova <aliya.kakimzhanova@nu.edu.kz>
2022-07-05 17:41:30 +0600

    first commit

---
+++ Cinema Room REST Service/task/src/cinema/Seat.java
@@ -0,0 +1,53 @@
+package cinema;
+
+public class Seat {
+    private int row;
+    private int column;
+
+    private int price;
+
+    public Seat(int row, int column) {
+        this.row = row;
+        this.column = column;
+        this.price = (row<=4 ? 10:8);
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Seat seat = (Seat) o;
+
+        if (row != seat.row) return false;
+        return column == seat.column;
+    }
+
+
+}
